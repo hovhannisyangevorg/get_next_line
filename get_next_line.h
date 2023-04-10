316ca32dd2c9d0b75f6bd93140466534b67e9ab2
Gevorg Hovhannisyan <gehovhan@c1r4s4.42yerevan.am>
2023-04-10 15:29:11 +0400

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehovhan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 12:55:34 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/02/14 12:55:36 by gehovhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+#include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE	100
+
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_move_start(char	*start);
+char	*ft_readed_line(char *start);
+char	*ft_strjoin(char *start, char *buff);
+char	*ft_join(char *dest, char *s1, char *s2);
+char	*ft_strchr(char *string, int searchedChar);
+
+size_t	ft_strlen(char *str);
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehovhan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 18:20:25 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/02/15 18:22:48 by gehovhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+size_t	ft_strlen(char *str);
+char	*get_next_line(int fd);
+char	*ft_move_start(char *start);
+char	*ft_readed_line(char *src);
+char	*ft_strchr(char *string, int searchedChar);
+char	*ft_strjoin(char *start, char *buff);
+char	*ft_join(char *dest, char *s1, char *s2);
+
+#endif
